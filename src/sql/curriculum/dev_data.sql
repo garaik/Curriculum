386eda1e6650d0fb99297ed72ce59acc26fff621
Adam Szarka <adam.szarka@p92.hu>
2013-11-09 22:00:26 +0100

    Added insert script for the first user

---
+++ src/sql/curriculum/dev_data.sql
@@ -0,0 +1,8 @@
+/*
+This script will insert an admin user into the curriculum database
+user: admin00
+pass: admin00
+*/
+INSERT INTO USER (ID, VERSION, ACTIVE, EMAIL, LOGIN, NAME, PASSWORD, ROLE)
+    VALUES
+        (1, 1, 1, 'admin@admin.hu', 'admin00', 'Admin', 'r+P/v8ywRwUT2/NBH3AC5trB83g=', 'admin')
